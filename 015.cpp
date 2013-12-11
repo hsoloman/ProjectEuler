48beb9f66f279e4d1630f7885c025969b30c2bcd
hsoloman <hsoloman@gmail.com>
2013-12-11 16:46:49 -0500

    Create 015.cpp

---
+++ 015.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   long long sum = 1;
+     
+   for (int i = 1 ; i <=20 ; i++){
+       sum *= (i + 20);
+	   sum /= i;
+   }
+   
+   cout << sum;
+
+   return 0;
+}
